5380e0d2aabc2f2d4e0e9821ef7b7e41df41c804
JonasGN <j.goncalvesneto@outlook.com>
2022-03-30 17:21:07 -0300

    [add] smooth scroll for internal links

--- scripts/script.js
+++ scripts/script.js
@@ -51,3 +51,27 @@ function initAccordionFaq() {
 }
 
 initAccordionFaq();
+
+function initScrollToSection() {
+  const internaLinks = document.querySelectorAll(".js-menu a[href^='#']");
+  
+  if (internaLinks.length) {
+    internaLinks.forEach((link) => {
+      link.addEventListener('click', scrollToSection)
+    });
+  }
+
+  function scrollToSection(event) {
+    event.preventDefault();
+
+    const href = this.getAttribute("href");
+    const section = document.querySelector(href);
+
+    window.scrollTo({
+      top: section.offsetTop,
+      behavior: 'smooth',
+    });
+  }
+}
+
+initScrollToSection();
